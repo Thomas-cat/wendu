2bf088448963224950a1834ee55a0756ea8d44da
谢俊杰 <xiejunjie@xiejunjies-Pro.lan>
2018-09-29 09:17:13 +0800

    test

--- Enrollproject/enroll/forms.py
+++ Enrollproject/enroll/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 class StudentForm(forms.Form):
 	name = forms.CharField(max_length = 20,strip=True,required=True)
-	qq = forms.CharField(max_length = 12,min_length=6,strip=True)
-	obj_school = forms.CharField(max_length = 100,strip=True)
-	institute = forms.CharField(max_length = 100,strip=True,required=True)
-	major = forms.CharField(max_length = 100,strip=True)
+	qq = forms.CharField(max_length = 12,min_length=6,strip=True,required=False)
+	obj_school = forms.CharField(max_length = 100,strip=True,required=False)
+	institute = forms.CharField(max_length = 100,strip=True,required=False)
+	major = forms.CharField(max_length = 100,strip=True,required=False)
 	phone = forms.CharField(max_length=11,min_length=11,strip=True,required=True)
 	is_enroll = forms.BooleanField(required=False)
 	fields = ['name','qq','obj_school','institute','major','phone','is_enroll']
--- Enrollproject/enroll/models.py
+++ Enrollproject/enroll/models.py
@@ -2,11 +2,13 @@ from django.db import models
 class Student(models.Model):
 	name = models.CharField(max_length = 20,verbose_name='姓名',blank=False)
 	qq = models.CharField(max_length = 12,blank=True)
-	obj_school = models.CharField(max_length = 100,verbose_name = '目标学校')
-	institute = models.CharField(max_length = 100,verbose_name = '所在学院')
+	obj_school = models.CharField(max_length = 100,verbose_name = '目标学校',blank=True)
+	institute = models.CharField(max_length = 100,verbose_name = '所在学院',blank = True)
 	major = models.CharField(max_length = 100,blank=True,verbose_name = '所在专业')
 	phone = models.CharField(max_length=11,blank=False,verbose_name = '手机号')
 	is_enroll = models.BooleanField(blank=True,verbose_name = '是否学员')
 	modifed_date = models.DateTimeField(auto_now_add=True,verbose_name ='信息提交时间')
 	class Meta:
 		 verbose_name_plural='学生报名信息表'
+	def __str__(self):	 	
+		return self.name
